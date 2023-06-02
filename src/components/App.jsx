66e5976a30bccbcdafb64d71115d1af46fb1a48a
Swann Julien <swann.jn@gmail.com>
2023-06-03 00:18:52 +0200

    set up folder organisation

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Info from "./Info.jsx";
+import About from "./About.jsx";
+import Interest from "./Interest.jsx";
+import Footer from "./Footer.jsx";
+
+function App() {
+  return (
+    <>
+      <Info />
+      <About />
+      <Interest />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
