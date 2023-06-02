f9c9f77ecaf9fb321c397acc8bf5f0d0e3b7dfd1
Swann Julien <swann.jn@gmail.com>
2023-06-02 23:48:11 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Hello world!</h1>
+      <img src="./src/assets/photo_swann.png" />
+    </>
+  );
+}
+
+export default App;
